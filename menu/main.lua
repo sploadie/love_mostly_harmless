6a3604819198785ed012c86fccafb74b284edd03
Antonin RIDOLFI <aridolfi@student.42.fr>
2016-12-10 16:07:20 +0100

    center buttons

--- menu/main.lua
+++ menu/main.lua
@@ -1,15 +1,14 @@
-require "menu"
+StartMenu = require "menu"
 
 gift = {}
 
 function love.load()
-	medium = love.graphics.newFont(45)
+	-- medium = love.graphics.newFont(45)
 	gamestate = "start_menu"
 
 	love.graphics.setBackgroundColor(255, 255, 255)
-
-	button_spawn(love.graphics.getWidth() / 2 - 135, love.graphics.getHeight() / 2 + 25, "Start Game", "start")
-	button_spawn(love.graphics.getWidth() / 2 - 150, love.graphics.getHeight() / 2 + 85, "Leave Game", "quit")
+	StartMenu:button_spawn(love.graphics.getWidth() / 2, love.graphics.getHeight() / 2 - 70, "Start Game", "start")
+	StartMenu:button_spawn(love.graphics.getWidth() / 2, love.graphics.getHeight() / 2, "Leave Game", "quit")
 end
 
 function love.update(dt)
@@ -18,21 +17,20 @@ function love.update(dt)
 	if gamestate == "playing" then
 
 	elseif gamestate == "start_menu" then
-		button_check()
+		StartMenu:button_check()
 	end
 end
 
 function love.draw()
 	if gamestate == "playing" then
-		if key == 'escape' then
-			gamestate = "pause"
+
 	elseif gamestate == "start_menu" then
-		button_draw()
+		StartMenu:button_draw()
 	end
 end
 
 function love.mousepressed(x, y)
 	if gamestate == "start_menu" then
-		button_click(x, y)
+		StartMenu:button_click(x, y)
 	end
 end
--- menu/menu.lua
+++ menu/menu.lua
@@ -1,27 +1,34 @@
-button = {}
+Menu = {}
+Menu.buttons = {}
+Menu.font = love.graphics.newFont(45)
 
-function button_spawn(x, y, text, id)
-	table.insert(button, {x = x, y= y, text = text, id = id, mouse_over = false})
+function Menu:button_spawn(x, y, text, id, font)
+	local font = font or self.font
+	local x = x - font:getWidth(text)/2
+	table.insert(self.buttons, {x = x, y = y, text = text, id = id, font = font, mouse_over = false})
 end
 
-function button_draw()
-	for i,v in ipairs(button) do
+function Menu:button_draw()
+	for i,v in ipairs(self.buttons) do
+		local font = v.font or self.font
+		love.graphics.setFont(font)
 		if v.mouse_over == false then
 			love.graphics.setColor(0, 0, 0)
 		else
 			love.graphics.setColor(198, 0, 0)
 		end
-		love.graphics.setFont(medium)
 		love.graphics.print(v.text, v.x, v.y)
 	end
 end
 
-function button_click(x, y)
-	for i,v in ipairs(button) do
+function Menu:button_click(x, y)
+	for i,v in ipairs(self.buttons) do
+		local font = v.font or self.font
+		love.graphics.setFont(font)
 		if x > v.x and
-		x < v.x + medium:getWidth(v.text) and
+		x < v.x + font:getWidth(v.text) and
 		y > v.y and
-		y < v.y + medium:getHeight(v.text) then
+		y < v.y + font:getHeight(v.text) then
 			if v.id == "quit" then
 				love.event.push("quit")
 			elseif v.id == "start" then
@@ -31,15 +38,19 @@ function button_click(x, y)
 	end
 end
 
-function button_check()
-	for i,v in ipairs(button) do
+function Menu:button_check()
+	for i,v in ipairs(self.buttons) do
+		local font = v.font or self.font
+		love.graphics.setFont(font)
 		if mouse_x > v.x and
-		mouse_x < v.x + medium:getWidth(v.text) and
+		mouse_x < v.x + font:getWidth(v.text) and
 		mouse_y > v.y and
-		mouse_y < v.y + medium:getHeight(v.text) then
+		mouse_y < v.y + font:getHeight(v.text) then
 			v.mouse_over = true
 		else
 			v.mouse_over = false
 		end
 	end
 end
+
+return Menu
